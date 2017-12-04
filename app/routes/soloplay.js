1efa619f9a3078fa8d11f5695ff43ce36a74a093
Elizabeth Nguyen <syaora@csu.fullerton.edu>
2017-12-03 21:46:45 -0800

    Added function that observes for any route changes

--- app/routes/soloplay.js
+++ app/routes/soloplay.js
@@ -1,13 +1,13 @@
 import Ember from 'ember';
 var jQuery = Ember.$;
 
-function getMovieInfo(movieTitle){
-  var requestURL = "http://www.omdbapi.com/?apikey=d0537451&t="+movieTitle;
+function getMovieInfo(movieTitle) {
+  var requestURL = "http://www.omdbapi.com/?apikey=d0537451&t=" + movieTitle;
   var request = new XMLHttpRequest();
-  request.open("GET",requestURL,false);
+  request.open("GET", requestURL, false);
   request.send(null);
-  if (request.status === 200){
-        return request.responseText;
+  if (request.status === 200) {
+    return request.responseText;
   }
 }
 
@@ -17,7 +17,7 @@ function getMovieInfo(movieTitle){
 
 }*/
 
-function compareScores(movieRoundList){
+function compareScores(movieRoundList) {
 
   var rating1 = movieRoundList[0].rating;
   var rating2 = movieRoundList[1].rating;
@@ -28,97 +28,118 @@ function compareScores(movieRoundList){
 
   var largestIndex = 0;
 
-  if((rating1 == rating2 && votes1 < votes2) || rating1 < rating2){
-    largestIndex=1;
+  if ((rating1 == rating2 && votes1 < votes2) || rating1 < rating2) {
+    largestIndex = 1;
   }
-  if((rating1 == rating3 && votes1 < votes3) || rating1 < rating3){
-    largestIndex=2;
+  if ((rating1 == rating3 && votes1 < votes3) || rating1 < rating3) {
+    largestIndex = 2;
   }
 
   return largestIndex;
 }
 
-function getThreeMovieTitles(masterMovieList){
+function getThreeMovieTitles(masterMovieList) {
   var movieArray = [];
-  for(var i=0; i<3;i++){
-    var randomMovieIndex = Math.floor(Math.random()*masterMovieList.length);
-    var randomMovie = masterMovieList[randomMovieIndex-1];
-    movieArray[i]=randomMovie;
+  for (var i = 0; i < 3; i++) {
+    var randomMovieIndex = Math.floor(Math.random() * masterMovieList.length);
+    var randomMovie = masterMovieList[randomMovieIndex - 1];
+    movieArray[i] = randomMovie;
   }
   return movieArray;
 }
 
 //test
-function getModelData(){
-  var movieRoundList=[];
-var masterMovieList={};
-jQuery.ajax({
-      url: 'movie_list.json',
-      success: function (result) {
-        masterMovieList=result;
-      },
-      async: false
+function getModelData() {
+  var movieRoundList = [];
+  var masterMovieList = {};
+  jQuery.ajax({
+    url: 'movie_list.json',
+    success: function(result) {
+      masterMovieList = result;
+    },
+    async: false
   });
-var threeMovieTitles = getThreeMovieTitles(masterMovieList);
-//var movieTitle3 = 'Nightmare+Before+Christmas';
-//var movieTitle2 = 'Corpse+Bride';
-//var movieTitle1 = 'Frankenweenie';
-var responseText1 = getMovieInfo(threeMovieTitles[0].Title);
-var responseText2 = getMovieInfo(threeMovieTitles[1].Title);
-var responseText3 = getMovieInfo(threeMovieTitles[2].Title);
+  var threeMovieTitles = getThreeMovieTitles(masterMovieList);
+  //var movieTitle3 = 'Nightmare+Before+Christmas';
+  //var movieTitle2 = 'Corpse+Bride';
+  //var movieTitle1 = 'Frankenweenie';
+  var responseText1 = getMovieInfo(threeMovieTitles[0].Title);
+  var responseText2 = getMovieInfo(threeMovieTitles[1].Title);
+  var responseText3 = getMovieInfo(threeMovieTitles[2].Title);
 
-var movieObject1 = JSON.parse(responseText1);
-var movieObject2 = JSON.parse(responseText2);
-var movieObject3 = JSON.parse(responseText3);
+  var movieObject1 = JSON.parse(responseText1);
+  var movieObject2 = JSON.parse(responseText2);
+  var movieObject3 = JSON.parse(responseText3);
 
-var reformatedMovieObject1 = {title:movieObject1.Title,
-                              poster:movieObject1.Poster,
-                              rating:movieObject1.imdbRating,
-                              votes:movieObject1.imdbVotes,
-                              bestMovie:"false"};
-var reformatedMovieObject2 = {title:movieObject2.Title,
-                              poster:movieObject2.Poster,
-                              rating:movieObject2.imdbRating,
-                              votes:movieObject2.imdbVotes,
-                              bestMovie:"false"};
-var reformatedMovieObject3 = {title:movieObject3.Title,
-                              poster:movieObject3.Poster,
-                              rating:movieObject3.imdbRating,
-                              votes:movieObject3.imdbVotes,
-                              bestMovie:"false"};
+  var reformatedMovieObject1 = {
+    title: movieObject1.Title,
+    poster: movieObject1.Poster,
+    rating: movieObject1.imdbRating,
+    votes: movieObject1.imdbVotes,
+    bestMovie: "false"
+  };
+  var reformatedMovieObject2 = {
+    title: movieObject2.Title,
+    poster: movieObject2.Poster,
+    rating: movieObject2.imdbRating,
+    votes: movieObject2.imdbVotes,
+    bestMovie: "false"
+  };
+  var reformatedMovieObject3 = {
+    title: movieObject3.Title,
+    poster: movieObject3.Poster,
+    rating: movieObject3.imdbRating,
+    votes: movieObject3.imdbVotes,
+    bestMovie: "false"
+  };
 
-/*setMovieImgContainerAttributes(movieObject1.Title,
-                                movieObject1.Poster,
-                                movieObject1.imdbRating,
-                                movieObject1.imdbVotes);
-setMovieImgContainerAttributes(movieObject2.Title,
-                                movieObject2.Poster,
-                                movieObject2.imdbRating,
-                                movieObject2.imdbVotes);
-setMovieImgContainerAttributes(movieObject3.Title,
-                                movieObject3.Poster,
-                                movieObject3.imdbRating,
-                                movieObject3.imdbVotes);
-*/
-movieRoundList.push(reformatedMovieObject1);
-movieRoundList.push(reformatedMovieObject2);
-movieRoundList.push(reformatedMovieObject3);
+  /*setMovieImgContainerAttributes(movieObject1.Title,
+                                  movieObject1.Poster,
+                                  movieObject1.imdbRating,
+                                  movieObject1.imdbVotes);
+  setMovieImgContainerAttributes(movieObject2.Title,
+                                  movieObject2.Poster,
+                                  movieObject2.imdbRating,
+                                  movieObject2.imdbVotes);
+  setMovieImgContainerAttributes(movieObject3.Title,
+                                  movieObject3.Poster,
+                                  movieObject3.imdbRating,
+                                  movieObject3.imdbVotes);
+  */
+  movieRoundList.push(reformatedMovieObject1);
+  movieRoundList.push(reformatedMovieObject2);
+  movieRoundList.push(reformatedMovieObject3);
 
-var bestMovieIndex = compareScores(movieRoundList);
-movieRoundList[bestMovieIndex].bestMovie="true";
-return movieRoundList;
+  var bestMovieIndex = compareScores(movieRoundList);
+  movieRoundList[bestMovieIndex].bestMovie = "true";
+  return movieRoundList;
 }
 
 export default Ember.Route.extend({
-  model(){
+  model() {
     var movieList = getModelData();
     return movieList;
   },
   actions: {
-    refresh(){
-    this.location.reload(true);
-  }
-  }
+    refresh() {
+      this.location.reload(true);
+    }
+  },
+  /* Observes any route changes */
+  //router: Ember.inject.service("-routing"),
+  onRouteChange: Ember.observer('router.currentRouteName', function() {
+    this.controllerFor('soloplay').set('isDisabled', true);
+    this.controllerFor('soloplay').set('gameNotStarted', false);
+    this.controllerFor('soloplay').set('roundFinished', false);
+    this.controllerFor('soloplay').set('timeForNextRound', true);
+    this.controllerFor('soloplay').set('roundPoints', 0);
+    this.controllerFor('soloplay').set('multiplier', 0);
+    this.controllerFor('soloplay').set('roundNumber', 0);
+    this.controllerFor('soloplay').set('gameFinalScore', 0);
+    this.controllerFor('soloplay').set('firstName', '');
+    this.controllerFor('soloplay').set('lastName', '');
+    this.controllerFor('gameFinished', true);
+  })
 });
 
 /*
